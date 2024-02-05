2ad1fc4168478e172522767f8b2c5f4900ba66e9
Bill Wright <bill@wwwright.com>
2024-02-05 09:14:03 -0700

    Setup for in-class demo

---
+++ src/main/java/inclassdemo/Animal.java
@@ -0,0 +1,15 @@
+package inclassdemo;
+
+public class Animal {
+    String name;
+    String foodType;
+    String location;
+
+    public Animal(String name, String foodType, String location) {
+        this.name = name;
+        this.foodType = foodType;
+        this.location = location;
+    }
+
+
+}
