b68179547249629fc5a56c2e7642b72ee16e1e7a
Bill Wright <bill@wwwright.com>
2024-10-29 08:33:09 -0600

    Added options for calculating cost of beverage for Decorator Pattern

--- src/main/java/examples/decorator/Mocha.java
+++ src/main/java/examples/decorator/Mocha.java
@@ -1,6 +1,18 @@
 package examples.decorator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mocha extends BeverageDecorator {
+
+    private static Map<Size, Double> ADDITIONAL_COSTS;
+
+    static {
+        ADDITIONAL_COSTS = new HashMap<>();
+        ADDITIONAL_COSTS.put(Size.TALL, 0.00);
+        ADDITIONAL_COSTS.put(Size.GRANDE, 0.10);
+        ADDITIONAL_COSTS.put(Size.VENTI, 0.20);
+    }
 
     public Mocha(Beverage beverage) {
         super(beverage);
@@ -9,14 +21,27 @@ public class Mocha extends BeverageDecorator {
     }
 
     public Double getCost() {
-        Double totalCost = beverage.getCost();
-        if (getSize() == Size.TALL) {
-            return totalCost + cost;
-        } else if (getSize() == Size.GRANDE) {
-            return totalCost + cost + 0.10;
-        } else {
-            return totalCost + cost + 0.20;
+        Double totalCost = beverage.getCost() + cost;
+        if (getSize() == Size.VENTI) {
+            totalCost += 0.20;
         }
+        if (getSize() == Size.GRANDE) {
+            totalCost += 0.10;
+        }
+        return totalCost;
+    }
+
+    public Double getCost2() {
+        Double totalCost = beverage.getCost() + cost;
+        switch (getSize()) {
+            case GRANDE -> totalCost += 0.20;
+            case VENTI -> totalCost += 0.10;
+        }
+        return totalCost;
+    }
+
+    public Double getCost3() {
+        return beverage.getCost() + ADDITIONAL_COSTS.get(getSize());
     }
 
 }
